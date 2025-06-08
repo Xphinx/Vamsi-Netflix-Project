e8e38c67c1ad4886e5ffccfc47ff4678b08a5cb0
A J <tjacques>
2025-06-08 11:51:22 -0400

    first push to github

--- Vamsi_git_ec2.tf
+++ Vamsi_git_ec2.tf
@@ -1,2 +1,3 @@
 # code to create and EC2 instance
 #type = "t2.micro"
+## This is a Terraform configuration file for creating an EC2 instance in AWS.Module "ec2_instance" {
--- Vamsi_iam-user.tf
+++ Vamsi_iam-user.tf
@@ -1,1 +1,2 @@
 # Creating an IAM user with the name "vamsi" and attaching the "AdministratorAccess" policy
+# additionally, creating an access key for the user
