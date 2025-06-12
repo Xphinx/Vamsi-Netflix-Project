55caded140232b357fdb065de58d5fddc8e6bdc1
A J <tjacques>
2025-06-11 20:06:50 -0400

    modified for task-20

---
+++ Vamsi_S3.tf
@@ -0,0 +1,2 @@
+#bucket name is --> netflix-project-<JT>
+#Access level is private
--- Vamsi_git_ec2.tf
+++ Vamsi_git_ec2.tf
@@ -1,3 +1,4 @@
 # code to create and EC2 instance
 #type = "t2.micro"
 ## This is a Terraform configuration file for creating an EC2 instance in AWS.Module "ec2_instance" {
+#OS of the system is windows
